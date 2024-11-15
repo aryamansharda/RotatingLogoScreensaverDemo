acc32759152f556b10efb037a1a8433796eb6d16
aryamansharda <asharda@turo.com>
2024-11-14 17:21:23 -0800

    Initial Commit

---
+++ RotatingLogo/RotatingLogoView.h
@@ -0,0 +1,12 @@
+//
+//  RotatingLogoView.h
+//  RotatingLogo
+//
+//  Created by Aryaman Sharda on 11/14/24.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface RotatingLogoView : ScreenSaverView
+
+@end
---
+++ RotatingLogo/RotatingLogoView.m
@@ -0,0 +1,51 @@
+//
+//  RotatingLogoView.m
+//  RotatingLogo
+//
+//  Created by Aryaman Sharda on 11/14/24.
+//
+
+#import "RotatingLogoView.h"
+
+@implementation RotatingLogoView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
